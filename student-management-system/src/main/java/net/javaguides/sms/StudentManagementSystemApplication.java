df02884320786705d8c64ace0a19c6aad4ee657d
yashsganorkar <167862474+yashsganorkar@users.noreply.github.com>
2024-10-06 13:20:12 +0530

    Add files via upload
    commit

---
+++ student-management-system/src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,26 @@
+package net.javaguides.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.javaguides.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+		System.out.println("Application Started...");
+	}
+    
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
